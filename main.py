2ca0464975e8c06b81d81cbaba894f7c1a3d02f2
unknown <gitrepowizard@gmail.com>
2021-01-18 15:56:51 -0600

    removed dead, unnecessary code

--- main.py
+++ main.py
@@ -8,32 +8,10 @@ from flask import Flask, request, render_template, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from newspaper import Article
-# hello test git
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SECRET_KEY'] = 'yo'
 db = SQLAlchemy(app)
-# one user has many posts, one post only has 1 user,
-# 1 to many model
-cnn_index = 0
-dw_index = 0
-wing = ''
-cnn_links = [
-            'https://www.cnn.com/2021/01/02/politics/senate-republicans-electoral-college/index.html',
-             'https://www.cnn.com/2021/01/03/politics/trump-republicans-electoral-college-new-congress-democracy/index.html',
-             'https://www.cnn.com/2021/01/01/politics/biden-history-analysis/index.html',
-             'https://www.cnn.com/2020/12/19/politics/student-loan-relief-devos-trump-biden/index.html',
-             'https://www.cnn.com/2020/12/22/politics/biden-trump-refugee-cap/index.html'
-]
-
-dw_links = [
-    'https://www.dailywire.com/news/ted-cruz-other-gop-senators-sign-letter-announcing-intent-to-vote-against-electors-from-disputed-states-until-election-audit-is-conducted',
-            'https://www.dailywire.com/news/warren-schumer-invoke-econ-101-in-push-to-cancel-50k-in-fed-student-loan-debt-per-borrower',
-            'https://www.dailywire.com/news/biden-will-raise-number-of-refugees-admitted-to-u-s-from-15k-to-125k',
-            'https://www.dailywire.com/news/the-powerful-consequences-of-2020-election-how-it-could-shape-federal-agencies',
-            'https://www.dailywire.com/news/mcconnells-home-vandalized-after-vote-to-enhance-stimulus-checks-blocked'
-            ]
-
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -59,21 +37,6 @@ class News(db.Model):
     title = db.Column(db.Text)
     reader_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-posts = [
-    {
-        'author': 'Nikhil Gaur',
-        'title':  'I love Dallas Mavericks',
-        'body': 'I really love the Dallas Mavericks, I have been watching them since I was a child',
-        'date': '12/26/2020'
-    },
-    {
-        'author': 'Himanshu Gaur',
-        'title':  'Dallas Mavericks suck now',
-        'body': 'Dallas Mavericks have had a really rough go since their win in 2011, failing to get past the first round ever since, have not even made it into playoffs since then, Luka Doncic addition has been sort of helpful',
-        'date': '12/26/2020'
-    }
-]
-
 @app.route('/',methods=['GET','POST'])
 def index():
     if request.method == 'GET':
@@ -84,7 +47,6 @@ def index():
         password = request.form['password']
         users = User.query.filter_by(username=username).all()
         if len(users) > 0:
-            print('Successfully connected existing user')
             return redirect(url_for("home",username=username))
         else:
             return redirect(url_for("register"))
@@ -102,22 +64,17 @@ def register():
             user = User(username=username,password=password,email=email)
             db.session.add(user)
             db.session.commit()
-            print("Successfully added new user to system")
             flash("Successfully added new user to system")
             return redirect(url_for("home",username=username))
         else:
-            print("Error User Already exists")
             flash(f"User {username} already exists in the system, this action is for new users","error")
             return render_template('register.html')
 
 @app.route('/home/<username>',methods=['GET','POST'])
 def home(username):
-    global wing
-    print("rendering home template")
     user = User.query.filter_by(username=username).first()
     if request.method == 'POST':
         if 'text' in request.form and 'title' in request.form:
-            print('Getting text and title')
             text = request.form['text']
             title = request.form['title']
             flash("You have successfully made a post on PoliticAI!",'success')
@@ -125,8 +82,7 @@ def home(username):
             post.wing = get_wing(text)
             db.session.add(post)
             db.session.commit()
-        else:
-            print('Getting news info')
+        if 'news_link' in request.form:
             link = request.form['news_link']
             article = Article(link)
             article.download()
@@ -135,13 +91,12 @@ def home(username):
             keywords = ''
             for word in article.keywords:
                 keywords += word
-                keywords += ','
+                keywords += ', '
             news_article = News(keywords=keywords,description=article.meta_description,summary=article.summary,title=article.title,reader_id=user.id)
             db.session.add(news_article)
             db.session.commit()
-            choose_article = True
     posts = Post.query.all()
-    return render_template('home.html',username=username,posts=posts,num_posts=len(posts),news_articles=user.articles)
+    return render_template('home.html',username=username,posts=posts,news_articles=user.articles)
 
 def get_wing(text):
     embedding_model = SentenceTransformer(model_name_or_path='bert-base-nli-mean-tokens',
